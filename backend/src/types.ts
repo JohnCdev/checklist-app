eb62c74df9764317139f59f0552caecd93453047
John Carver <johncarverp@gmail.com>
2023-10-24 18:59:33 -0400

    added update and delete routes

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -2,3 +2,12 @@ export interface CreateNoteBody {
   title?: string;
   content?: string;
 }
+
+export interface UpdateNoteParams {
+  noteId: string;
+}
+
+export interface UpdateNoteBody {
+  title?: string;
+  content?: string;
+}
