c5d1d13c26e4f9d5e302c7399eecb1018892f57d
John Carver <johncarverp@gmail.com>
2023-10-30 19:28:25 -0400

    note formatting and date formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Note as NoteModel } from "./models/note";
 import Note from "./components/Note";
+import { Container, Row } from "react-bootstrap";
+import styles from "./styles/NotesPage.module.css";
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
@@ -23,9 +25,13 @@ function App() {
 
   return (
     <div>
-      {notes.map((note) => {
-        return <Note note={note} key={note._id} />;
-      })}
+      <Container>
+        <Row xs={1} md={2} xl={3} className="g-4">
+          {notes.map((note) => {
+            return <Note note={note} className={styles.note} key={note._id} />;
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -2,21 +2,31 @@ import React from "react";
 import styles from "../styles/Note.module.css";
 import { Note as NoteModel } from "../models/note";
 import { Card } from "react-bootstrap";
+import { formatDate } from "../utils/formatDate";
 
 interface NoteProps {
   note: NoteModel;
+  className?: string;
 }
 
-const Note = ({ note }: NoteProps) => {
+const Note = ({ note, className }: NoteProps) => {
   const { title, content, createdAt, updatedAt } = note;
+
+  let updatedAtText: string;
+  if (updatedAt > createdAt) {
+    updatedAtText = "Updated: " + formatDate(updatedAt);
+  } else {
+    updatedAtText = "Created: " + formatDate(createdAt);
+  }
 
   return (
     <div>
-      <Card className={styles.noteCard}>
-        <Card.Body>
+      <Card className={`${styles.noteCard} ${className}`}>
+        <Card.Body className={styles.cardBody}>
           <Card.Title>{title}</Card.Title>
           <Card.Text className={styles.cardText}>{content}</Card.Text>
         </Card.Body>
+        <Card.Footer className="text-muted">{updatedAtText}</Card.Footer>
       </Card>
     </div>
   );
